639341e177cc724ec0203804ad3e529d3365dd7b
Emmanuel Arias <emmanuelarias30@gmail.com>
2018-08-15 21:13:26 -0300

    [WIP] ConfigFilesManager

--- coremanagement/src/ConfigFilesManager/configFilesManager.py
+++ coremanagement/src/ConfigFilesManager/configFilesManager.py
@@ -1,6 +1,10 @@
 import os
 from ConfigFileManager.ConfigException import ConfigException
+from ConfigFileManager.ConfigException import KeyNotInConfigFile
+from ConfigFileManager.ConfigException import NotValidValueConfig
+from ConfigFileManager.ConfigException import RequiredNotExiste
 import configparser
+
 
 class configFilesManager(object):
     def __init__(self, path_to_config: str, dict_config_allows: str):
@@ -16,17 +20,48 @@ class configFilesManager(object):
         if not isinstance(str, self.dict_config_allows):
             raise TypeError("dict_config_allows must be a dict()")
 
-
     def ___read_config(self):
         self.config = configparser.ConfigParser()
         self.config.read(self.path_to_config)
 
+    def ___update_parser_config_dict(self, key_config, key2_dict,
+                                     value2_dict, required=False):
+        try:
+            if value2_dict['type'] == 'int':
+                self.parser_config_dict[key_config][key2_dict] = \
+                    self.config.getint(key_config, key2_dict)
+                flag_used = True
+
+            elif value2_dict['type'] == 'float':
+                self.parser_config_dict[key_config][key2_dict] = \
+                    self.config.getfloat(key_config, key2_dict)
+                flag_used = True
+
+            elif value2_dict['type'] == 'bool':
+                self.parser_config_dict[key_config][key2_dict] = \
+                    self.config.getboolean(key_config, key2_dict)
+                flag_used = True
+
+            elif value2_dict['type'] == 'str':
+                self.parser_config_dict[key_config][key2_dict] = \
+                    self.config[key_config][key2_dict]
+                flag_used = True
+            else:
+                self.parser_config_dict[key_config][key2_dict] = \
+                    self.config[key_config][key2_dict]
+                flag_used = True
+        except KeyError as keyerror:
+            raise KeyNotInConfigFile("{} Does not exist on Config File "
+                                     "this is required".format(keyerror))
+        except ValueError as valueerror:
+            raise NotValidValueConfig('Not valid Type: {}'.format(valueerror))
+
     def is_correct_information(self):
-        dont_exist_any_sections = True
         for key_dict, values_dict in self.dict_config_allows:
             for key_config in list(self.config.sections()):
-                if key_dict in key_config:
-                    for key2_dict, values2_dict in values_dict:
+                if key_dict == key_config:
+                    for key2_dict, values2_dict in \
+                            self.dict_config_allows[key_config]:
                         options = [opt for opt in self.config[key_config]]
 
                         # If this section doesn't have any option, I assert
@@ -35,14 +70,17 @@ class configFilesManager(object):
 
                         if key2_dict in options and values2_dict['required'] \
                                 == True:
-                            if values2_dict['type'] == 'int':
-                                self.parser_config_dict[key_config][
-                                    key2_dict] = self.config[key_config][
-                                    key2_dict].getint()
-
-                        else:
-                            break
-
-                    dont_exist_any_sections = False
-                else:
-                    break
+                            # First check the required values
+                            try:
+                                self.___update_parser_config_dict(key_config,
+                                                                  key2_dict,
+                                                                  values2_dict)
+                            except KeyNotInConfigFile as keynoterror:
+                                raise RequiredNotExiste('Required '\
+                                     'value: {}'.format(keynoterror))
+                        if key2_dict in options and values2_dict['required']\
+                            == False:
+                            # Then check the not required values
+                            self.___update_parser_config_dict(key_config,
+                                                              key2_dict,
+                                                              values2_dict)
